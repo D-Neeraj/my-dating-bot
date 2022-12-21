9577a5e9700b656bb332aae80ee92bfa81a1c07c
Neerja <neerajdayalan@gmail.com>
2022-12-21 12:31:31 +0530

    :construction: comments added

--- main.py
+++ main.py
@@ -1,46 +1,12 @@
 # Importing the DataCleaning class from the data_cleaning.py file.
-# from service.data_cleaning import DataCleaning
-#
-# gender = input("Please select your gender - (Male | M, Female F): ")
-# your_nick_name = input("Please enter your nick name: ")
-# print(f"Diamond Jackson: Hello {your_nick_name}!!!, welcome to our heaven...")
-# data_clean = DataCleaning.getInstance()
-# # This is a loop that will keep asking the user for input and then print the cleaned message.
-# while True:
-#     message = input(f"{your_nick_name}: ")
-#     cleaned_message = data_clean.clean_message(message)
-#     print(f"Diamond Jackson: {cleaned_message}")
-#
-#
+from service.data_cleaning import DataCleaning
 
-from dataset import *
-import json
-
-x=json.loads(entity.jason)
-
-
-
-# conversion to JSON done by dumps() function
-b = json.dumps(a)
-
-# printing the output
-print(b)
-
-aws = [
-    {
-        "entity_name": "horny_detect",
-        "entity_values": {
-            "value": "horny",
-            "synonyms": [
-                "mood",
-                "desiring",
-                "hardened up",
-                "hot",
-                "high",
-                "erect"
-            ]
-        }
-    },
-]
-
-
+gender = input("Please select your gender - (Male | M, Female F): ")
+your_nick_name = input("Please enter your nick name: ")
+print(f"Diamond Jackson: Hello {your_nick_name}!!!, welcome to our heaven...")
+data_clean = DataCleaning.getInstance()
+# This is a loop that will keep asking the user for input and then print the cleaned message.
+while True:
+    message = input(f"{your_nick_name}: ")
+    cleaned_message = data_clean.clean_message(message)
+    print(f"Diamond Jackson: {cleaned_message}")
